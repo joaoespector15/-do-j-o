4dc7952c13bee993c95873e68bb439772c8378a0
joaoespector15 <abreu.silva.joao@escola.pr.gov.br>
2024-10-15 15:50:20 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(600, 600);
+    background("white")
+  }
+  
+  
+  function draw() {
+    
+    
+    stroke("gray");
+    fill("black");
+    
+    // console.log(mouseIsPressed);
+    
+    if(mouseIsPressed) {
+      
+    }  
